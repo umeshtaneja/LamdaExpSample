1ac659514514b5df9b2149de3ed5c2b502eb08c3
umesh <umeshtaneja.123@gmail.com>
2018-02-12 01:39:01 +0530

    First Commit

---
+++ src/One.java
@@ -0,0 +1,9 @@
+
+ public class One {
+     public static void main(String[] args) {
+         Runnable runnable = () -> System.out.println("running thread...");
+         runnable.run();
+     }
+ }
+
+
